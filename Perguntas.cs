f732f6b27824e8070e7796ee00ea1900ad59c692
AMAURI LEONARDI <108447480+AmauriLeonardi@users.noreply.github.com>
2025-07-08 20:14:51 -0300

    Add files via upload

---
+++ Perguntas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooPerguntaEResposta
+{
+	public class Pergunta
+	{
+		public Pergunta()
+		{
+			this.Texto = "";
+			this.Resposta = "";
+        }
+
+        public Pergunta(string texto, string resposta )
+        {
+            this.Texto = "";
+            this.Resposta = "";
+        }
+
+
+        private string Texto { get; set; }
+
+        public string Resposta { get; set; }
+
+	} 
+}
